82eb686ca713510a987ba401c342380ea7190380
Lana <lana@lana.com>
2024-08-04 22:32:05 -0400

    primer ejercicio con js

---
+++ assets/js/calcular.js
@@ -0,0 +1,26 @@
+var first = Number(prompt("Ingrese el primer número"));
+this.isNum(first);
+var secund = Number(prompt("Ingrese el segundo número"));
+this.isNum(secund);
+first == secund ? this.msg(1) : first > secund ? this.msg(2) : this.msg(3);
+
+function isNum(num) {
+    while (!Number.isInteger(num)) {
+        num = Number(prompt("Reingrese el número"));
+    }
+}
+
+function msg(option) {
+    switch (option) {
+        case 1:
+            alert("Los números son iguales "+ "1er número: " + first + " 2do número: " + secund);
+            console.log("=") 
+            break;
+        case 2:
+            alert("1er número: " + first + " es mayor que el 2do número: " + secund);
+            break;
+        case 3:
+            alert("2do número: " + secund + " es mayor que el 1er número: " + first);
+            break;
+    }
+}
